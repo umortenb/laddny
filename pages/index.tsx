db87adb29e139e0c59256b93403eb967a1238518
umbiermann <ulfmortenbiermann@gmail.com>
2020-11-01 18:52:40 +0100

    Set up Next.js with typescript and styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const HomePage = () => {
+  return ( 
+    <Title>Test</Title>
+   );
+}
+ 
+export default HomePage;
