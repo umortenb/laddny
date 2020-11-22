6d205cc08a4dddbac66d41764791b5362ba7103f
umbiermann <ulfmortenbiermann@gmail.com>
2020-11-22 18:36:11 +0100

    Implement main theme and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import { AuthContextProvider } from "../components/auth/AuthContextProvider"
 import { getApolloClient } from "../lib/apolloClient"
 import { ApolloProvider } from "@apollo/client"
 import { AppProps } from "next/dist/next-server/lib/router/router"
+import { ThemeProvider } from "styled-components"
+import { mainTheme } from "../view/mainTheme"
+import { GlobalStyle } from "../view/GlobalStyle"
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <AuthContextProvider>
       <ApolloProvider client={getApolloClient}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </ApolloProvider>
     </AuthContextProvider>
   )
