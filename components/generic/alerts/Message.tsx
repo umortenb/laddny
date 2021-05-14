63c770ff869d783ca8b59b885cbceb5944397d65
umbiermann <ulfmortenbiermann@gmail.com>
2021-05-14 09:20:30 +0200

    Change theme colors

--- components/generic/alerts/Message.tsx
+++ components/generic/alerts/Message.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
-import { accentColor, errorColor } from "../../styles/colorPalettes";
 
 export const Message = styled.div`
   padding: 10px;
   background-color: ${(props) =>
-    props.type === "error" ? errorColor[200] : accentColor[200]};
+    props.type === "error"
+      ? props.theme.errorColor[200]
+      : props.theme.accentColor[200]};
   border: 1px solid;
   border-color: ${(props) =>
-    props.type === "error" ? errorColor[300] : accentColor[300]};
+    props.type === "error"
+      ? props.theme.errorColor[300]
+      : props.theme.accentColor[300]};
   border-radius: 5px;
 
   display: flex;
--- components/styles/mainTheme.tsx
+++ components/styles/mainTheme.tsx
@@ -1,9 +1,15 @@
-import { accentColor, neutralColor, primaryColor } from "./colorPalettes";
+import {
+  accentColor,
+  errorColor,
+  neutralColor,
+  primaryColor,
+} from "./colorPalettes";
 
 export const mainTheme = {
   primaryColor: primaryColor,
   neutralColor: neutralColor,
   accentColor: accentColor,
-  backgroundColor: neutralColor[200],
-  headerBackgroundColor: neutralColor[200],
+  errorColor: errorColor,
+  backgroundColor: primaryColor[100],
+  headerBackgroundColor: primaryColor[100],
 };
