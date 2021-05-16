f9f169558b41ff9b28e60716b089252f0282176d
umbiermann <ulfmortenbiermann@gmail.com>
2021-05-16 14:18:29 +0200

    Add modern-normalize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import AuthProvider from "../components/auth/AuthProvider";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ThemeProvider } from "styled-components";
 import HeaderMenu from "../components/layout/HeaderMenu";
+import "modern-normalize/modern-normalize.css";
 import { mainTheme } from "../components/styles/mainTheme";
 import { GlobalStyle } from "../components/styles/GlobalStyle";
 import { PageContainer } from "../components/layout/PageContainer";
